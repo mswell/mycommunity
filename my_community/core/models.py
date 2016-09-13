8ba43f3bcd435e7d83b815670385cc2603e1ad25
Caio Carrara <caiocarrara@gmail.com>
2016-09-13 15:37:30 -0300

    Adds initial Business Model

--- my_community/core/models.py
+++ my_community/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class BusinessSegment(models.Model):
+    name = models.CharField(_('Name'), max_length=250)
+
+    def __str__(self):
+        return name
+
+
+class Business(models.Model):
+    SERVICE = 'service'
+    TRADE = 'trade'
+    BUSINESSES_TYPES = (
+        (SERVICE, _('Service')),
+        (TRADE, _('Trade')),
+    )
+    name = models.CharField(_('Name'), max_length=250, unique=True)
+    address = models.CharField(_('Address'), max_length=250)
+    latitude = models.IntegerField(_('Latitude'), blank=True, null=True)
+    longitude = models.IntegerField(_('Longitude'), blank=True, null=True)
+    email = models.EmailField(_('E-Mail'))
+    facebook_page = models.URLField(_('Facebook'), blank=True, null=True)
+    twitter_profile = models.URLField(_('Twitter'), blank=True, null=True)
+    linkedin_profile = models.URLField(_('LinkedIn'), blank=True, null=True)
+    business_type = models.CharField(_('Type'), max_length=250, blank=False,
+                                     null=False, default=TRADE,
+                                     choices=BUSINESSES_TYPES)
+    segment = models.ForeignKey(BusinessSegment, verbose_name=_('Segment'),
+                                on_delete=models.CASCADE, blank=False, null=False,
+                                related_name=u'businesses')
+
+    def __str__(self):
+        return "{} ({} - {})".format(self.name, self.segment, self.business_type)
--- my_community/core/tests.py
+++ my_community/core/tests.py
@@ -1,4 +1,20 @@
+from django.db import IntegrityError
 from django.test import TestCase
+
+from mixer.backend.django import mixer
+
+from core.models import Business
+
+
+class BusinessTestCase(TestCase):
+    def setUp(self):
+        mixer.cycle(5).blend(Business)
+
+    def test_uniqueness(self):
+        business = Business.objects.get(pk=1)
+        new_business = Business(name=business.name)
+        with self.assertRaises(IntegrityError):
+            new_business.save()
 
 
 class HomeTest(TestCase):
