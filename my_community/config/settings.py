0ad4cf8cdf70f53c50fc9871cd2f9fec79ed1c4a
Caio Carrara <caiocarrara@gmail.com>
2016-09-13 11:01:11 -0300

    Changes configuration folder name

--- my_community/config/settings.py
+++ my_community/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'my_community.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'my_community.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
